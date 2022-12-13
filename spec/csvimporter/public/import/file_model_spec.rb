62ef806f6ab668123fd2ab532478df88ba14dc87
Joel Azemar <joel.azemar@gmail.com>
2022-12-13 13:26:25 +0100

    Initial Commit

--- spec/csvimporter/public/import/file_model_spec.rb
+++ spec/csvimporter/public/import/file_model_spec.rb
@@ -2,35 +2,39 @@
 
 require "spec_helper"
 
-describe Csvimporter::Import::FileModel do
-  let(:context) { {} }
+module Csvimporter
+  module Import
+    describe FileModel do
+      let(:context) { {} }
 
-  describe "class" do
-    let(:import_model_klass) { FileImportModel }
+      describe "class" do
+        let(:import_model_klass) { FileImportModel }
 
-    describe "#header_matchers" do
-      subject { import_model_klass.header_matchers(context) }
+        describe "#header_matchers" do
+          subject(:header_matchers) { import_model_klass.header_matchers(context) }
 
-      let(:header_matchers) { [/^:: - string1 - ::$/i, /^:: - string2 - ::$/i] }
+          let(:expected_header_matchers) { [/^:: - string1 - ::$/i, /^:: - string2 - ::$/i] }
 
-      it { expect(subject).to eql header_matchers }
-    end
+          it { expect(header_matchers).to eql expected_header_matchers }
+        end
 
-    describe "#index_header_match" do
-      context "when is a match" do
-        subject { import_model_klass.index_header_match(some_cell, context) }
+        describe "#index_header_match" do
+          context "when is a match" do
+            subject(:index_header_match) { import_model_klass.index_header_match(some_cell, context) }
 
-        let(:some_cell) { ":: - string2 - ::" }
+            let(:some_cell) { ":: - string2 - ::" }
 
-        it { expect(subject).to be 1 }
-      end
+            it { expect(index_header_match).to be 1 }
+          end
 
-      context "when is not a match" do
-        subject { import_model_klass.index_header_match(some_cell, context) }
+          context "when is not a match" do
+            subject(:index_header_match) { import_model_klass.index_header_match(some_cell, context) }
 
-        let(:some_cell) { "String 3" }
+            let(:some_cell) { "String 3" }
 
-        it { expect(subject).to be_nil }
+            it { expect(index_header_match).to be_nil }
+          end
+        end
       end
     end
   end
