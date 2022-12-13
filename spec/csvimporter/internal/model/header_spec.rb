61bc37003c143103f703dc92c21879d3f067a2e6
Joel Azemar <joel.azemar@gmail.com>
2022-12-13 14:24:19 +0100

    Initial Commit

--- spec/csvimporter/internal/model/header_spec.rb
+++ spec/csvimporter/internal/model/header_spec.rb
@@ -2,32 +2,36 @@
 
 require "spec_helper"
 
-describe Csvimporter::Model::Header do
-  let(:instance) { described_class.new(:string1, row_model_class, string1: "context") }
-  let(:row_model_class) do
-    Class.new(BasicRowModel) do
-      def self.format_header(*args)
-        args.join("__")
-      end
-    end
-  end
-
-  describe "#value" do
-    subject { instance.value }
-
-    it "returns the formatted_header" do
-      expect(subject).to eql "string1__#<OpenStruct string1=\"context\">"
-    end
-
-    context "with :header option" do
+module Csvimporter
+  module Model
+    describe Header do
+      let(:instance) { described_class.new(:string1, row_model_class, string1: "context") }
       let(:row_model_class) do
         Class.new(BasicRowModel) do
-          column :string1, header: "waka"
+          def self.format_header(*args)
+            args.join("__")
+          end
         end
       end
 
-      it "returns the option value" do
-        expect(subject).to eql "waka"
+      describe "#value" do
+        subject(:value) { instance.value }
+
+        it "returns the formatted_header" do
+          expect(value).to eql "string1__#<OpenStruct string1=\"context\">"
+        end
+
+        context "with :header option" do
+          let(:row_model_class) do
+            Class.new(BasicRowModel) do
+              column :string1, header: "waka"
+            end
+          end
+
+          it "returns the option value" do
+            expect(value).to eql "waka"
+          end
+        end
       end
     end
   end
