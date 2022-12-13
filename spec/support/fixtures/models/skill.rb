da2b3b094f1c8e451d2b0cc084f5e6c511e2b122
Joel Azemar <joel.azemar@gmail.com>
2022-12-13 11:42:19 +0100

    Initial Commit

--- spec/support/fixtures/models/skill.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Skill
-  class << self
-    def all
-      %w[Organized Clean Punctual Strong Crazy Flexible]
-    end
-  end
-end
--- spec/support/fixtures/models/user.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-User = Struct.new(:first_name, :last_name, :skills)
