ac1ed7ef3ca97879244576e1ae37548b5c61b02a
Joel Azemar <joel.azemar@gmail.com>
2022-12-13 15:14:13 +0100

    Initial Commit

--- spec/csvimporter/concerns/model/base_spec.rb
+++ spec/csvimporter/concerns/model/base_spec.rb
@@ -2,19 +2,23 @@
 
 require "spec_helper"
 
-describe Csvimporter::Model::Base do
-  describe "instance" do
-    let(:options)  { {} }
-    let(:instance) { BasicRowModel.new(options) }
+module Csvimporter
+  module Model
+    describe Base do
+      describe "instance" do
+        let(:options)  { {} }
+        let(:instance) { BasicRowModel.new(options) }
 
-    describe "#initialized_at" do
-      subject { instance.initialized_at }
+        describe "#initialized_at" do
+          subject(:initialized_at) { instance.initialized_at }
 
-      let(:date_time) { DateTime.now }
+          let(:date_time) { DateTime.now }
 
-      it "gives the time" do
-        expect(DateTime).to receive(:now).and_return(date_time)
-        expect(subject).to eql date_time
+          it "gives the time" do
+            allow(DateTime).to receive(:now).and_return(date_time)
+            expect(initialized_at).to eql date_time
+          end
+        end
       end
     end
   end
