b89620b554a522d75af9d3ee9e0071cface37ac8
Joel Azemar <joel.azemar@gmail.com>
2022-12-13 16:32:37 +0100

    Initial Commit

--- spec/csvimporter/concerns/import/base_spec.rb
+++ spec/csvimporter/concerns/import/base_spec.rb
@@ -12,17 +12,17 @@ module Csvimporter
         let(:instance)        { row_model_class.new(source_row, options) }
 
         describe "#valid?" do
-          subject { instance.valid? }
+          subject(:import_validation) { instance.valid? }
 
           it "defaults to true" do
-            expect(subject).to be true
+            expect(import_validation).to be true
           end
 
           context "with Exception given" do
             let(:instance) { row_model_class.new(StandardError.new("msg")) }
 
             it "is invalid and has empty row as source" do
-              expect(subject).to be false
+              expect(import_validation).to be false
               expect(instance.errors.full_messages).to eql ["Csv has msg"]
               expect(instance.source_row).to eql []
             end
@@ -30,20 +30,20 @@ module Csvimporter
         end
 
         describe "#source_attributes" do
-          subject { instance.source_attributes }
+          subject(:source_attributes) { instance.source_attributes }
 
           it "returns a map of `column_name => source_row[index_of_column_name]" do
-            expect(subject).to eql({ alpha: "alpha", beta: "beta" })
+            expect(source_attributes).to eql({ alpha: "alpha", beta: "beta" })
           end
         end
 
         describe "#free_previous" do
-          subject { instance.free_previous }
+          subject(:free_previous) { instance.free_previous }
 
           let(:options) { { previous: row_model_class.new([]) } }
 
           it "makes previous nil" do
-            expect { subject }.to change(instance, :previous).to(nil)
+            expect { free_previous }.to change(instance, :previous).to(nil)
           end
 
           context "when the class depends on the previous.previous" do
@@ -64,23 +64,23 @@ module Csvimporter
         end
 
         describe "#skip?" do
-          subject { instance.skip? }
+          subject(:import_skip) { instance.skip? }
 
           it "is false when valid" do
-            expect(subject).to be false
+            expect(import_skip).to be false
           end
 
           it "is true when invalid" do
             expect(instance).to receive(:valid?).and_return(false)
-            expect(subject).to be true
+            expect(import_skip).to be true
           end
         end
 
         describe "#abort?" do
-          subject { instance.skip? }
+          subject(:import_abort) { instance.abort? }
 
           it "is always false" do
-            expect(subject).to be false
+            expect(import_abort).to be false
           end
         end
       end
