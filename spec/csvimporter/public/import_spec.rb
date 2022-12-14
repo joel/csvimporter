ae1013493f927b38780d26b386d9aec81209e04a
Joel Azemar <joel.azemar@gmail.com>
2022-12-14 16:03:31 +0100

    Initial Commit

--- spec/csvimporter/public/import_spec.rb
+++ spec/csvimporter/public/import_spec.rb
@@ -4,6 +4,23 @@ require "spec_helper"
 
 module Csvimporter
   describe Import do
-    it_behaves_like "with_or_without_csv_row_model_model", described_class
+    context "without including Csvimporter::Model" do
+      let(:klass) do
+        Class.new do
+          include Import
+
+          column :alpha
+          column :beta
+        end
+      end
+
+      it do
+        expect(klass.new(%w[alpha beta]).attributes).to eql(alpha: "alpha", beta: "beta")
+      end
+
+      it "has Csvimporter::Model included" do
+        expect(klass.ancestors).to include Csvimporter::Model
+      end
+    end
   end
 end
