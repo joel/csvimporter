bd15cf21722295d548e65c1a5686dc642b9da330
Joel Azemar <joel.azemar@gmail.com>
2022-12-15 14:32:42 +0100

    Initial Commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,12 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "csvimporter"
 
 require "#{Dir.pwd}/spec/support/shared_context/with_context.rb"
+
 Dir["#{Dir.pwd}/spec/support/**/*.rb"].sort.each { |f| require f }
 
 RSpec.configure do |c|
   c.run_all_when_everything_filtered = true
 
   c.include CsvFilePaths
-  c.include WithThisThenContext
+  c.include WithContext
 end
